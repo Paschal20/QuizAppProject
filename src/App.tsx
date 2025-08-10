0448f89c42ff18ce9010db26d291bf49a904e893
Paschal Anaedu <170808501@live.unilag.edu.ng>
2025-08-10 19:52:16 +0100

    not found page added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import StartPage from "./pages/StartPage";
 import QuizPage from "./pages/QuizPage";
 import LeaderboardPage from "./pages/LeaderboardPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route path="/" element={<StartPage />} />
       <Route path="/quiz" element={<QuizPage />} />
       <Route path="/leaderboard" element={<LeaderboardPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
