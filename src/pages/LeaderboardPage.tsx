d267c15314f39f1ce1c511b233f2b4106d10b5be
Paschal Anaedu <170808501@live.unilag.edu.ng>
2025-08-10 17:43:22 +0100

    third commit a

--- src/pages/LeaderboardPage.tsx
+++ src/pages/LeaderboardPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import  { useEffect, useMemo, useState } from "react";
 
 type Entry = { id: number; name: string; score: number; date?: string };
 
