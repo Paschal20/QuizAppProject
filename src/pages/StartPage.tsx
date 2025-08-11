f5344cfe5597e17c815d2c7b88fc5ad8f246860e
soyinkanaomi <soyinkanaomi@gmai.com>
2025-08-11 12:57:12 +0100

    naomio changes

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -11,6 +11,7 @@ export default function StartPage() {
     navigate("/quiz");
   };
   // this is a return
+  // this is naomi changes
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-3xl font-bold mb-6">Mini Quiz Game</h1>
